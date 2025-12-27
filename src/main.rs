f5ab66c5cc21493abd684e3a6d241cfb5a1cef48
theroguevigilante <naimish82@proton.me>
2025-12-28 00:55:44 +0530

    Add new routes

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,26 @@ use tower_http::services::ServeDir;
 
 use app::client_type;
 use routes::home;
+use routes::articles;
+use routes::blog;
+use routes::contact;
 
 #[tokio::main]
 async fn main() {
     let addr = "127.0.0.1:6432";
     let router = Router::new()
         .route("/", get(home::handler))
+        .nest(
+            "/articles",
+            Router::new()
+                .route("/", get(articles::list_handler))
+        )
+        .nest(
+            "/blog",
+            Router::new()
+                .route("/", get(blog::list_handler))
+        )
+        .route("/contact", get(contact::handler))
         .nest_service("/static", ServeDir::new("static"))
         .route("/client_test", get(client_type::handler));
     let listener = TcpListener::bind(addr).await.unwrap();
