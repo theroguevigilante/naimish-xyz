ae9d3a42464c901707228a395f06978b2a9546ad
theroguevigilante <naimish82@proton.me>
2025-12-28 11:34:05 +0530

    Add a new field page_name in index

--- src/routes/home.rs
+++ src/routes/home.rs
@@ -7,6 +7,7 @@ use axum::response::Html;
 #[template(path = "index.html")]
 struct IndexTemplate<'a> {
     site_title: &'a str,
+    page_name: &'a str,
     site_description: &'a str,
     recent_posts: Vec<Post>,
     num_of_posts: usize,
@@ -14,7 +15,8 @@ struct IndexTemplate<'a> {
 
 pub async fn handler() -> Html<String> {
     let page = IndexTemplate {
-        site_title: "naimish.xyz",
+        site_title: "Naimish",
+        page_name: "naimish.xyz",
         site_description: "This is my personal site. Rewritten in rust. As always I am going to be using this for documenting my projects. Have a good time :)",
         recent_posts: recent_posts(),
         num_of_posts: 5,
