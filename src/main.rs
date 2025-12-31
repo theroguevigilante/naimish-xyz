ad21aef1f656dbe3ce2c1549952cd9761f3ca154
theroguevigilante <naimish82@proton.me>
2025-12-31 23:19:06 +0530

    Oopsies

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ use routes::contact;
 #[tokio::main]
 async fn main() {
     let port = std::env::var("PORT").unwrap_or_else(|_| "6432".into() );
-    let addr = format!("127.0.0.1:{}", port);
+    let addr = format!("0.0.0.0:{}", port);
     let router = Router::new()
         .route("/", get(home::handler))
         .nest(
